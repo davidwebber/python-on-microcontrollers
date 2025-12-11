3250acdd9e76cfda4e7ca6ee4f2fb801d0e52b2b
David Webber <david@scanalyticsinc.com>
2025-12-11 13:50:29 -0600

    add counter to blink_led

--- Blink_LED/code.py
+++ Blink_LED/code.py
@@ -6,9 +6,11 @@ led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 delay = 0.5
+i = 0
 while True:
-    print("Hello World!")
+    print(f"Hello World! {i}")
     led.value = True
     time.sleep(delay)
     led.value = False
     time.sleep(delay)
+    i += 1
